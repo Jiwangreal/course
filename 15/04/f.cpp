22bf985962e30c5de6f7811f380ff2bb051dc8ce
JiWangreal <jiwangreal@163.com>
2022-07-24 08:38:37 -0700

    2022-7-24

--- 15/04/f.cpp
+++ 15/04/f.cpp
@@ -5,7 +5,7 @@
 using namespace std;
 
 int main() {
-    string s = "+03.14e-03"s;
+    string s = "+03.14e-03"s;//string s = "+03.14E-03"s
     cout << "字符串: " << s << endl;
     float f = stof(s);
     cout << "浮点数: " << f << endl;
