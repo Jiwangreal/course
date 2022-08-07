360b0550651dadcd1afaa02870726e596e4d7793
JiWangreal <jiwangreal@163.com>
2022-08-07 08:18:22 -0700

    2022-8-7 more

---
+++ 11/template/main.c
@@ -0,0 +1,10 @@
+// #include <cstdio>
+#include <stdio.h>
+struct Test{};
+
+int main() {
+    struct Test t_Test;
+    printf("sizeof(Test): %lu\n", sizeof(struct Test));
+    printf("Hello, world!\n");
+    return 0;
+}
